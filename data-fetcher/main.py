194f1956914738150f9237321cfca545b94c9ced
Daniel Lavin <dlavinv64@hotmail.com>
2021-01-12 22:30:24 +0100

    Data fetcher initial commit

---
+++ data-fetcher/main.py
@@ -0,0 +1,15 @@
+import os
+
+class DataFetcher:
+    def __init__(self):
+        pass
+
+    def run(self):
+        self._fetch_tweets()
+        self._fetch_economics()
+
+    def _fetch_tweets(self):
+        pass
+
+    def _fetch_economics(self):
+        pass
